fb0732ce730de19973fc1d7001aadfe79ac28978
korkeep <korkeep@naver.com>
2021-06-15 09:46:30 +0900

    NUCLEO-L552ZE-Q Firmware Flashing

--- PX4-Trust/NUCLEO-L552ZE-Q/NuttX/nuttx_update.sh
+++ PX4-Trust/NUCLEO-L552ZE-Q/NuttX/nuttx_update.sh
@@ -9,18 +9,14 @@ stm32programmercli="STM32_Programmer_CLI"
 connect_no_reset="-c port=SWD mode=UR"
 connect="-c port=SWD mode=UR --hardRst"
 
-slot1=0x08000000
-image_number=1
+flash=0x08000000
 
-# Sungsu Added
-if [ $image_number == 1 ]; then
 echo "Write NuttX"
-#$stm32programmercli $connect -d $SCRIPTPATH/nuttx.bin $slot1 -v
-$stm32programmercli $connect -d $SCRIPTPATH/nuttx.bin $slot1 -v
+$stm32programmercli $connect -d $SCRIPTPATH/nuttx.bin $flash -v
 ret=$?
-# if [ $ret != 0 ]; then
-#   if [ "$1" != "AUTO" ]; then read -p "SBSFU_UPDATE script failed, press a key" -n1 -s; fi
-#   exit 1
-# fi
+if [ $ret != 0 ]; then
+  if [ "$1" != "AUTO" ]; then read -p "SBSFU_UPDATE script failed, press a key" -n1 -s; fi
+  exit 1
+fi
 echo "NuttX Written"
 fi
